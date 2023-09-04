1a8ec4afb85c2b2ffbcf7e508ddfe29c8fdfc45f
aileks <88669313+aileks@users.noreply.github.com>
2023-09-04 09:53:33 -0400

    Lock script is now properly themed.

--- .config/scripts/lock.sh
+++ .config/scripts/lock.sh
@@ -1,3 +1,54 @@
-#!/bin/bash
+#!/bin/sh
 
-i3lock -i $HOME/Pictures/lockscreen.png
+alpha='dd'
+background='#282a36'
+selection='#44475a'
+comment='#6272a4'
+
+yellow='#f1fa8c'
+orange='#ffb86c'
+red='#ff5555'
+magenta='#ff79c6'
+blue='#6272a4'
+cyan='#8be9fd'
+green='#50fa7b'
+
+i3lock \
+  --insidever-color=$selection$alpha \
+  --insidewrong-color=$selection$alpha \
+  --inside-color=$selection$alpha \
+  --ringver-color=$green$alpha \
+  --ringwrong-color=$red$alpha \
+  --ringver-color=$green$alpha \
+  --ringwrong-color=$red$alpha \
+  --ring-color=$blue$alpha \
+  --line-uses-ring \
+  --keyhl-color=$magenta$alpha \
+  --bshl-color=$orange$alpha \
+  --separator-color=$selection$alpha \
+  --verif-color=$green \
+  --wrong-color=$red \
+  --layout-color=$blue \
+  --date-color=$blue \
+  --time-color=$blue \
+  --screen 1 \
+  --blur 1 \
+  --clock \
+  --indicator \
+  --time-str="%H:%M:%S" \
+  --date-str="%A %e %B %Y" \
+  --verif-text="Checking..." \
+  --wrong-text="Wrong pswd" \
+  --noinput="No Input" \
+  --lock-text="Locking..." \
+  --lockfailed="Lock Failed" \
+  --radius=120 \
+  --ring-width=10 \
+  --pass-media-keys \
+  --pass-screen-keys \
+  --pass-volume-keys \
+  --time-font="FantasqueSansM Nerd Font" \
+  --date-font="FantasqueSansM Nerd Font" \
+  --layout-font="FantasqueSansM Nerd Font" \
+  --verif-font="FantasqueSansM Nerd Font" \
+  --wrong-font="FantasqueSansM Nerd Font"
