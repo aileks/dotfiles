833eac57875bd59328cf4c489a8ab89206434cdf
aileks <88669313+aileks@users.noreply.github.com>
2023-09-25 10:04:12 -0400

    Tweaks

--- .config/scripts/startup.sh
+++ .config/scripts/startup.sh
@@ -2,13 +2,11 @@
 
 # Important
 /usr/bin/lxpolkit &
-/usr/bin/xss-lock --transfer-sleep-lock -- ~/.config/scripts/lock.sh --nofork &            
-/usr/bin/dunst > /dev/null 2>&1 &                                                              
-/usr/bin/picom --config ~/.config/picom/picom.conf -b &                                    
-setxkbmap -option numpad:mac &                                                                 
+/usr/bin/xss-lock --transfer-sleep-lock -- ~/.config/scripts/lock.sh --nofork &
+/usr/bin/dunst > /dev/null 2>&1 &
+/usr/bin/picom --config ~/.config/picom/picom.conf -b &
 
 # Personal
 feh --no-fehbg --bg-scale ~/.bg.png &
-flameshot &                                                                                    
-/usr/bin/xmodmap ~/.Xmodmap
-thunderbird &
+flameshot &
+setxkbmap -option numpad:mac &
