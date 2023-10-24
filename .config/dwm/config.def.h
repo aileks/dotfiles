c8cee59a4f1c882eb67593ea1332612b0c445f74
aileks <88669313+aileks@users.noreply.github.com>
2023-10-24 10:23:25 -0400

    Added phpactor and tmux configs. Changed termcmd keybind to launch kitty with tmux session.

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -5,26 +5,20 @@ static const unsigned int borderpx = 2; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const unsigned int gappih = 6;   /* horiz inner gap between windows */
 static const unsigned int gappiv = 6;   /* vert inner gap between windows */
-static const unsigned int gappoh =
-    6; /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov =
-    6; /* vert outer gap between windows and screen edge */
-static int smartgaps =
-    0; /* 1 means no outer gap when there is only one window */
-static const unsigned int systraypinning =
-    0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
+static const unsigned int gappoh = 6; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov = 6; /* vert outer gap between windows and screen edge */
+static int smartgaps = 0; /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
 X */
-static const unsigned int systrayonleft =
-    0; /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayonleft = 0; /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2; /* systray spacing */
-static const int systraypinningfailfirst =
-    1; /* 1: if pinning fails, display systray on the first monitor, False:
+static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display systray on the first monitor, False:
 display systray on the last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
 static const int showbar = 1;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
-static const char *fonts[] = {"FiraCode Nerd Font Propo:size=10"};
-static const char dmenufont[] = "FiraCode Nerd Font Propo:size=10";
+static const char *fonts[] = {"CaskaydiaCove NF:size=10"};
+static const char dmenufont[] = "CaskaydiaCove NF:size=10";
 static const char col_bg[] = "#babbf1";
 static const char col_bg2[] = "#303446";
 static const char col_border[] = "#babbf1";
@@ -46,20 +40,16 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    {"Firefox", NULL, NULL, 1 << 8, 0, -1},
     {"Thunderbird", NULL, NULL, 4, 0, -1},
 };
 
 /* layout(s) */
 static const float mfact = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;    /* number of clients in master area */
-static const int resizehints =
-    1; /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen =
-    1; /* 1 will force focus on the fullscreen window */
+static const int resizehints = 1; /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-#define FORCE_VSPLIT                                                           \
-  1 /* nrowgrid layout: force two clients to always split vertically */
+#define FORCE_VSPLIT 1 /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
@@ -99,7 +89,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *roficmd[] = {"rofi",  "-show", "drun", NULL};
-static const char *termcmd[] = {"kitty", NULL};
+static const char *termcmd[] = {"kitty", "tmux", NULL};
 static const char *browsercmd[] = {"mercury-browser", NULL};
 static const char *filemgr[] = {"thunar", NULL};
 static const char *volupcmd[] = {"pamixer", "-i", "5", NULL};
--- .config/scripts/status.sh
+++ .config/scripts/status.sh
@@ -1,7 +1,7 @@
 #!/bin/env sh
 
-update_cpu () { 
-    	read -r cpu a b c previdle rest < /proc/stat
+update_cpu () {
+	read -r cpu a b c previdle rest < /proc/stat
 	prevtotal=$((a+b+c+previdle))
 	sleep 0.5
 	read -r cpu a b c idle rest < /proc/stat
@@ -9,24 +9,24 @@ update_cpu () {
 	cpu=" 󰻠 $((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))%"
 }
 
-update_memory () { 
-	memory="  $(free --giga -h | sed -n "2s/\([^ ]* *\)\{2\}\([^ ]*\).*/\2/p")"
+update_memory () {
+	memory="   $(free --giga -h | sed -n "2s/\([^ ]* *\)\{2\}\([^ ]*\).*/\2/p")"
 }
 
-update_time () { 
-	time="  $(date "+%a %b %d %I:%M %P")"
+update_time () {
+	time="   $(date "+%a %b %d %I:%M %P")"
 }
 
-update_bat () { 
+update_bat () {
 	read -r bat_capacity </sys/class/power_supply/BAT1/capacity
 	bat="󰁹 $bat_capacity%"
 }
 
-update_vol () { 
-	vol="  $(pamixer --get-volume-human)"
+update_vol () {
+	vol="   $(pamixer --get-volume-human)"
 }
 
-display () { 
+display () {
 	xsetroot -name " $bat  | $cpu  | $memory  | $vol  | $time "
 }
 
@@ -35,15 +35,13 @@ trap "update_bat;display" "RTMIN+2"
 
 while true
 do
-    sleep 1 & wait && { 
-	# to update item every n seconds with a offset of m
-	## [ $((sec % n)) -eq m ] && udpate_item
-	[ $((sec % 1 )) -eq 0 ] && update_cpu
-	[ $((sec % 5 )) -eq 0 ] && update_memory
-	[ $((sec % 60 )) -eq 0 ] && update_time
-	[ $((sec % 5 )) -eq 0 ] && update_vol
-	[ $((sec % 60)) -eq 0 ] && update_bat
-	[ $((sec % 5 )) -eq 0 ] && display
-	sec=$((sec + 1))
-    }
-done 
+	sleep 1 & wait && {
+		[ $((sec % 1 )) -eq 0 ] && update_cpu
+			[ $((sec % 5 )) -eq 0 ] && update_memory
+			[ $((sec % 30 )) -eq 0 ] && update_time
+			[ $((sec % 5 )) -eq 0 ] && update_vol
+			[ $((sec % 60)) -eq 0 ] && update_bat
+			[ $((sec % 5 )) -eq 0 ] && display
+			sec=$((sec + 1))
+		}
+	done
