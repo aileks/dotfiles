477fe9792a0f6d289ea3cb436a7271cda791d720
aileks <88669313+aileks@users.noreply.github.com>
2023-10-24 16:09:04 -0400

    Fixed and cleaned up neo-tree config

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -119,8 +119,31 @@ require('lazy').setup({
             'nvim-lua/plenary.nvim',
             'MunifTanjim/nui.nvim',
             { 'nvim-tree/nvim-web-devicons', opts = true },
+            {
+                's1n7ax/nvim-window-picker',
+                config = function()
+                    require 'window-picker'.setup({
+                        filter_rules = {
+                            include_current_win = false,
+                            autoselect_one = true,
+                            bo = {
+                                filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+                                buftype = { 'terminal', "quickfix" },
+                            },
+                        },
+                    })
+                end,
+            },
         },
         config = function()
+            vim.fn.sign_define("DiagnosticSignError",
+              {text = " ", texthl = "DiagnosticSignError"})
+            vim.fn.sign_define("DiagnosticSignWarn",
+              {text = " ", texthl = "DiagnosticSignWarn"})
+            vim.fn.sign_define("DiagnosticSignInfo",
+              {text = " ", texthl = "DiagnosticSignInfo"})
+            vim.fn.sign_define("DiagnosticSignHint",
+              {text = "󰌵", texthl = "DiagnosticSignHint"})
             require('user/plugins/neo-tree')
         end,
     },
--- .config/nvim/lua/user/plugins/neo-tree.lua
+++ .config/nvim/lua/user/plugins/neo-tree.lua
@@ -1,29 +1,17 @@
 require('neo-tree').setup({
-    config = function()
-        vim.fn.sign_define('DiagnosticSignError',
-          {text = ' ', texthl = 'DiagnosticSignError'})
-        vim.fn.sign_define('DiagnosticSignWarn',
-          {text = ' ', texthl = 'DiagnosticSignWarn'})
-        vim.fn.sign_define('DiagnosticSignInfo',
-          {text = ' ', texthl = 'DiagnosticSignInfo'})
-        vim.fn.sign_define('DiagnosticSignHint',
-          {text = '󰌵', texthl = 'DiagnosticSignHint'})
-    end,
-
-    sources = { "filesystem", "buffers", "git_status" },
+    sources = { 'filesystem', 'buffers', 'git_status' },
     source_selector = {
         winbar = true,
-        content_layout = "center",
+        content_layout = 'center',
     },
-    close_if_last_window = true,
-    popup_border_style = 'rounded',
+
+    close_if_last_window = false,
     enable_git_status = true,
     enable_diagnostics = true,
     enable_normal_mode_for_inputs = false,
     open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' },
     sort_case_insensitive = false,
-    sort_function = nil ,
-
+    sort_function = nil,
 
     default_component_configs = {
         container = {
@@ -46,7 +34,7 @@ require('neo-tree').setup({
             folder_open = '',
             folder_empty = '󰜌',
             default = '*',
-            highlight = 'NeoTreeFileIcon',
+            highlight = 'NeoTreeFileIcon'
         },
         modified = {
             symbol = '[+]',
@@ -59,12 +47,10 @@ require('neo-tree').setup({
         },
         git_status = {
             symbols = {
-                -- Change type
-                added     = '✚',
-                modified  = '',
+                added     = '',
+                modified  = '',
                 deleted   = '✖',
                 renamed   = '󰁕',
-                -- Status type
                 untracked = '',
                 ignored   = '',
                 unstaged  = '󰄱',
@@ -72,10 +58,29 @@ require('neo-tree').setup({
                 conflict  = '',
             }
         },
+        file_size = {
+            enabled = true,
+            required_width = 64,
+        },
+        type = {
+            enabled = true,
+            required_width = 122,
+        },
+        last_modified = {
+            enabled = true,
+            required_width = 88,
+        },
+        created = {
+            enabled = true,
+            required_width = 110,
+        },
+        symlink_target = {
+            enabled = false,
+        },
     },
     window = {
         position = 'left',
-        width = 30,
+        width = 27,
         mapping_options = {
             noremap = true,
             nowait = true,
@@ -86,19 +91,17 @@ require('neo-tree').setup({
                 nowait = false,
             },
             ['<2-LeftMouse>'] = 'open',
-            ['<cr>'] = 'open',
+            ['<CR>'] = 'open',
             ['l'] = 'open',
-            ['<esc>'] = 'close',
-            ['h'] = 'close',
+            ['<Esc>'] = 'cancel',
             ['P'] = { 'toggle_preview', config = { use_float = true } },
             ['L'] = 'focus_preview',
             ['S'] = 'open_split',
             ['s'] = 'open_vsplit',
             ['t'] = 'open_tabnew',
             ['w'] = 'open_with_window_picker',
-            ['C'] = 'close_node',
+            ['h'] = 'close_node',
             ['z'] = 'close_all_nodes',
-            ['Z'] = 'expand_all_nodes',
             ['a'] = {
                 'add',
                 config = {
@@ -118,104 +121,98 @@ require('neo-tree').setup({
             ['?'] = 'show_help',
             ['<'] = 'prev_source',
             ['>'] = 'next_source',
-            ['i'] = 'show_file_details'
-        },
+            ['i'] = 'show_file_details',
+        }
     },
+    nesting_rules = {},
     filesystem = {
         filtered_items = {
             visible = true,
             hide_dotfiles = false,
             hide_gitignored = true,
+            hide_hidden = true,
         },
-    },
-    follow_current_file = {
-        enabled = true,
-        leave_dirs_open = true,
-    },
-    group_empty_dirs = false,
-    hijack_netrw_behavior = 'open_default',
-    use_libuv_file_watcher = false,
-
-    window = {
-        mappings = {
-            ['<bs>'] = 'navigate_up',
-            ['.'] = 'set_root',
-            ['H'] = 'toggle_hidden',
-            ['/'] = 'fuzzy_finder',
-            ['D'] = 'fuzzy_finder_directory',
-            ['#'] = 'fuzzy_sorter',
-            ['f'] = 'filter_on_submit',
-            ['<C-x>'] = 'clear_filter',
-
-            ['[g'] = 'prev_git_modified',
-            [']g'] = 'next_git_modified',
-            ['o'] = { 'show_help',
-            nowait=false,
-            config = { title = 'Order by', prefix_key = 'o' }},
-            ['oc'] = { 'order_by_created', nowait = false },
-            ['od'] = { 'order_by_diagnostics', nowait = false },
-            ['og'] = { 'order_by_git_status', nowait = false },
-            ['om'] = { 'order_by_modified', nowait = false },
-            ['on'] = { 'order_by_name', nowait = false },
-            ['os'] = { 'order_by_size', nowait = false },
-            ['ot'] = { 'order_by_type', nowait = false },
-        },
-        fuzzy_finder_mappings = {
-            ['<down>'] = 'move_cursor_down',
-            ['<C-n>'] = 'move_cursor_down',
-            ['<up>'] = 'move_cursor_up',
-            ['<C-p>'] = 'move_cursor_up',
-        },
-    },
-
-    buffers = {
         follow_current_file = {
             enabled = true,
             leave_dirs_open = true,
+        },
+        group_empty_dirs = true,
+        hijack_netrw_behavior = 'open_default',
+        use_libuv_file_watcher = false,
+        window = {
+            mappings = {
+                ['<BS>'] = 'navigate_up',
+                ['.'] = 'set_root',
+                ['H'] = 'toggle_hidden',
+                ['/'] = 'fuzzy_finder',
+                ['D'] = 'fuzzy_finder_directory',
+                ['#'] = 'fuzzy_sorter',
+                ['f'] = 'filter_on_submit',
+                ['<C-x>'] = 'clear_filter',
+                ['[g'] = 'prev_git_modified',
+                [']g'] = 'next_git_modified',
+                ['o'] = { 'show_help', nowait=false, config = { title = 'Order by', prefix_key = 'o' }},
+                ['oc'] = { 'order_by_created', nowait = false },
+                ['od'] = { 'order_by_diagnostics', nowait = false },
+                ['og'] = { 'order_by_git_status', nowait = false },
+                ['om'] = { 'order_by_modified', nowait = false },
+                ['on'] = { 'order_by_name', nowait = false },
+                ['os'] = { 'order_by_size', nowait = false },
+                ['ot'] = { 'order_by_type', nowait = false },
+            },
+            fuzzy_finder_mappings = {
+                ['<down>'] = 'move_cursor_down',
+                ['<C-n>'] = 'move_cursor_down',
+                ['<up>'] = 'move_cursor_up',
+                ['<C-p>'] = 'move_cursor_up',
+            },
+        },
+
+    },
+    buffers = {
+        follow_current_file = {
+            enabled = true,
+            leave_dirs_open = false,
         },
         group_empty_dirs = true,
         show_unloaded = true,
         window = {
             mappings = {
                 ['bd'] = 'buffer_delete',
-                ['<bs>'] = 'navigate_up',
+                ['<BS>'] = 'navigate_up',
                 ['.'] = 'set_root',
-                ['o'] = { 'show_help',
-                nowait=false,
-                config = { title = 'Order by', prefix_key = 'o' }},
+                ['o'] = { 'show_help', nowait=false, config = { title = 'Order by', prefix_key = 'o' }},
                 ['oc'] = { 'order_by_created', nowait = false },
                 ['od'] = { 'order_by_diagnostics', nowait = false },
                 ['om'] = { 'order_by_modified', nowait = false },
                 ['on'] = { 'order_by_name', nowait = false },
                 ['os'] = { 'order_by_size', nowait = false },
                 ['ot'] = { 'order_by_type', nowait = false },
-            },
-        },
-        git_status = {
-            window = {
-                position = 'float',
-                mappings = {
-                    ['A']  = 'git_add_all',
-                    ['gu'] = 'git_unstage_file',
-                    ['ga'] = 'git_add_file',
-                    ['gr'] = 'git_revert_file',
-                    ['gc'] = 'git_commit',
-                    ['gp'] = 'git_push',
-                    ['gg'] = 'git_commit_and_push',
-                    ['o'] = { 'show_help',
-                    nowait=false,
-                    config = { title = 'Order by', prefix_key = 'o' }},
-                    ['oc'] = { 'order_by_created', nowait = false },
-                    ['od'] = { 'order_by_diagnostics', nowait = false },
-                    ['om'] = { 'order_by_modified', nowait = false },
-                    ['on'] = { 'order_by_name', nowait = false },
-                    ['os'] = { 'order_by_size', nowait = false },
-                    ['ot'] = { 'order_by_type', nowait = false },
-                },
-            },
+            }
         },
     },
+    git_status = {
+        window = {
+            position = 'float',
+            mappings = {
+                ['A']  = 'git_add_all',
+                ['gu'] = 'git_unstage_file',
+                ['ga'] = 'git_add_file',
+                ['gr'] = 'git_revert_file',
+                ['gc'] = 'git_commit',
+                ['gp'] = 'git_push',
+                ['gg'] = 'git_commit_and_push',
+                ['o'] = { 'show_help', nowait=false, config = { title = 'Order by', prefix_key = 'o' }},
+                ['oc'] = { 'order_by_created', nowait = false },
+                ['od'] = { 'order_by_diagnostics', nowait = false },
+                ['om'] = { 'order_by_modified', nowait = false },
+                ['on'] = { 'order_by_name', nowait = false },
+                ['os'] = { 'order_by_size', nowait = false },
+                ['ot'] = { 'order_by_type', nowait = false },
+            }
+        }
+    }
 })
 
 vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>')
-vim.keymap.set('n', '<leader>o', '<C-w><C-w>') -- Yes, I know this is hacky...
+vim.keymap.set('n', '<leader>o', '<C-w><C-w>')
