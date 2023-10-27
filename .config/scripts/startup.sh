c50dc18701e8b73cde7d01846639722b61264e38
aileks <88669313+aileks@users.noreply.github.com>
2023-10-27 13:26:46 -0400

    script tweak

--- .config/scripts/startup.sh
+++ .config/scripts/startup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Important
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/bin/xss-lock --transfer-sleep-lock -- ~/.config/scripts/lock.sh --nofork &
 /usr/bin/dunst >/dev/null 2>&1 &
 /usr/bin/picom --config ~/.config/picom/picom.conf -b &
-/usr/bin/xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rate 144.00 --output HDMI-1-0 --primary --mode 1920x1080 --pos 1920x0 --rate 75.00 
+/usr/bin/xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rate 144.00 --output HDMI-1-0 --primary --mode 1920x1080 --pos 1920x0 --rate 75.00
 
 # Personal
 nm-applet &
