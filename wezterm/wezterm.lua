104be719f1c0a9989c7d3c8f3cd1b983c96f5db8
Aaliyah Harper <aeverly14@pm.me>
2025-01-25 19:24:35 -0500

    tweaks

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,6 @@ config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = false
 config.window_background_opacity = 0.95
 config.window_close_confirmation = 'NeverPrompt'
--- config.window_decorations = 'NONE'
+config.window_decorations = 'NONE'
 
 return config
