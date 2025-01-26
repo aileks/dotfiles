62a56902e8a8f5ecc6d422aca92ea59964ad20ec
Aaliyah Harper <aeverly14@pm.me>
2025-01-25 21:22:16 -0500

    theme updates

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -1,12 +1,10 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'nyoom-engineering/oxocarbon.nvim',
   priority = 1000,
   config = function ()
-    require('catppuccin').setup({
-      transparent_background = true
-    })
-
-    vim.cmd.colorscheme('catppuccin')
+    vim.cmd.colorscheme('oxocarbon')
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
   end
 }
