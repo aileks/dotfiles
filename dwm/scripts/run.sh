1a9c09b6522a2fe78dca3635f27e72fe3db287a0
Aaliyah Harper <aeverly14@pm.me>
2025-02-11 11:30:17 -0500

    no &

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -4,7 +4,7 @@ xrandr --output DP-2 --mode 2560x1440 --rate 165.08
 xrdb merge ~/.Xresources
 xset r rate 200 50
 xss-lock -- i3lock-fancy -pf BerkeleyMono-Nerd-Font &
-feh --no-fehbg --bg-fill ~/Pictures/Wallpaper/wallpaper.png &
+feh --no-fehbg --bg-fill ~/Pictures/Wallpaper/wallpaper.png
 dash ~/.config/dwm/scripts/bar.sh &
 picom -b --config ~/.config/picom/picom.conf
 fcitx5 -d
