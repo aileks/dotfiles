4dd7cda637a39d55fa952491818cf446310a7343
Aaliyah Harper <aeverly14@pm.me>
2025-02-12 20:16:56 -0500

    theme stuff

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,10 +5,10 @@ local config = {}
 -- actual config
 config.font = wezterm.font('BerkeleyMono Nerd Font')
 config.font_size = 14
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'Windows 10 (base16)'
 config.enable_tab_bar = false
 config.window_background_opacity = 0.95
 config.window_close_confirmation = 'NeverPrompt'
-config.window_decorations = 'NONE'
+-- config.window_decorations = 'NONE'
 
 return config
