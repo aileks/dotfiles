d3f8160873c4b44cf1c69025b9cce9b051f39f1b
Aaliyah Harper <aeverly14@pm.me>
2025-02-15 16:06:20 -0500

    changed some things

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ config.font = wezterm.font('BerkeleyMono Nerd Font')
 config.font_size = 14
 config.color_scheme = 'Windows 10 (base16)'
 config.enable_tab_bar = false
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.97
 config.window_close_confirmation = 'NeverPrompt'
 -- config.window_decorations = 'NONE'
 
