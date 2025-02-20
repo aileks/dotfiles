9e4f8c4d7236d23ea0d8b3d6fa6cf2fbba59a050
Aaliyah Harper <aeverly14@pm.me>
2025-02-20 11:40:54 -0500

    wezterm transparency update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ config.font = wezterm.font('BerkeleyMono Nerd Font')
 config.font_size = 14
 config.color_scheme = 'Wez'
 config.enable_tab_bar = false
-config.window_background_opacity = 0.93
+config.window_background_opacity = 0.90
 config.window_close_confirmation = 'NeverPrompt'
 -- config.window_decorations = 'NONE'
 
