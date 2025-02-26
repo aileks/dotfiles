17df29202786d65aa96cb0b61ccb6a7930d9b6a8
Aaliyah Harper <aeverly14@pm.me>
2025-02-25 20:46:32 -0500

    no more window maker

--- setup.sh
+++ setup.sh
@@ -8,22 +8,12 @@ DOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # symlinks
 ln -sf $DOTDIR/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $DOTDIR/zsh/zshrc $HOME/.zshrc
-rm -rf $HOME/.config/picom
-ln -s $DOTDIR/picom $HOME/.config/picom
 rm -rf $HOME/.config/wezterm
 ln -s $DOTDIR/wezterm $HOME/.config/wezterm
-rm -rf $HOME/.config/dunst
-ln -s $DOTDIR/dunst $HOME/.config/dunst
-rm -rf $HOME/.config/dwm
-ln -s $DOTDIR/dwm $HOME/.config/dwm
 rm -rf $HOME/.config/fastfetch
 ln -s $DOTDIR/fastfetch $HOME/.config/fastfetch
 rm -rf $HOME/.config/helix
 ln -s $DOTDIR/helix $HOME/.config/helix
-rm -rf $HOME/GNUstep/Library/WindowMaker/autostart
-ln -s $DOTDIR/GNUstep/Library/WindowMaker/autostart $HOME/GNUstep/Library/WindowMaker/autostart
-rm -rf $HOME/GNUstep/Library/WindowMaker/Backgrounds
-ln -s $DOTDIR/GNUstep/Library/WindowMaker/Backgrounds $HOME/GNUstep/Library/WindowMaker/Backgrounds
 
 # install omz
 echo "Installing Oh My Zsh"
