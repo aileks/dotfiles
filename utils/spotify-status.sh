062c30b7f90147589f871021a097586141da1842
Aaliyah Harper <aeverly14@pm.me>
2025-03-13 23:12:08 -0400

    formatting

--- utils/spotify-status.sh
+++ utils/spotify-status.sh
@@ -6,10 +6,10 @@ if playerctl --player=spotify status &>/dev/null; then
     title=$(playerctl --player=spotify metadata title)
 
     if [ -n "$artist" ] && [ -n "$title" ]; then
-	# Format what's currently playing
+    	# Format what's currently playing
         echo " %{F#9ccfd8} %{F-} ${artist} - ${title} "
     else
-	# Failsafe
+    	# Failsafe
         echo "%{F#eb6f92}No song information%{F-}"
     fi
 else
