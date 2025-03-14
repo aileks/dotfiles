67e2e5bae0fa231c5bb90ad112a08e298359697f
Aaliyah Harper <aeverly14@pm.me>
2025-03-13 20:47:32 -0400

    eye candy

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ config.font = wezterm.font('BerkeleyMono Nerd Font')
 config.font_size = 16
 config.color_scheme = 'rose-pine'
 config.enable_tab_bar = false
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.90
 config.window_close_confirmation = 'NeverPrompt'
 
 return config
