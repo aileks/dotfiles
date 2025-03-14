78647c58d0b5a7967feb422f201cb626d6f27486
Aaliyah Harper <aeverly14@pm.me>
2025-03-13 23:55:10 -0400

    more scripts

---
+++ utils/launch-session-menu.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Sigh...
+rofi -show p -modi p:"~/.local/bin/session-menu --symbols-font 'Material Icons'" -font "BerkeleyMono Nerd Font 18" -theme-str "window {width: 500px;}"
