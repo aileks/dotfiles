b6c539ef0a347dd82c1080efcfdc1db9d306fe52
Aaliyah Harper <aeverly14@pm.me>
2025-03-16 15:18:23 -0400

    back to wayland

--- utils/launch-session-menu.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Sigh...
-rofi -show p -modi p:"~/.local/bin/session-menu --symbols-font 'Material Icons'" -font "BerkeleyMono Nerd Font 18" -theme-str "window {width: 500px;}"
