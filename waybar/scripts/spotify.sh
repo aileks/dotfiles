888e8ab49a860b10f8132209e3e904204cf7498c
Aaliyah Harper <aeverly14@pm.me>
2025-03-16 17:46:19 -0400

    waybar configuration

--- waybar/scripts/spotify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-playerctl -p spotify -F metadata --format \
-'{{emoji(status)}} {{artist}} - {{title}}' 2>/dev/null | while read -r line; do
-    echo "{\"text\":\"$line\",\"class\":\"$([ $(playerctl status) = Playing ] && echo playing || echo paused)\"}"
-done
