387b870367449885cdaa4acc65bdc5980cc1cae3
Aaliyah Harper <aeverly14@pm.me>
2025-05-20 11:24:56 -0400

    broken sites

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -36,10 +36,10 @@ c.completion.open_categories = [
 # Appearance
 c.tabs.show = "multiple"
 c.colors.webpage.preferred_color_scheme = "dark"
-c.colors.webpage.darkmode.enabled = True
-c.colors.webpage.darkmode.algorithm = "lightness-cielab"
-c.colors.webpage.darkmode.threshold.background = 100
-c.colors.webpage.darkmode.policy.images = "smart"
+# c.colors.webpage.darkmode.enabled = True
+# c.colors.webpage.darkmode.algorithm = "lightness-cielab"
+# c.colors.webpage.darkmode.threshold.background = 100
+# c.colors.webpage.darkmode.policy.images = "smart"
 config.set("colors.webpage.darkmode.enabled", False, "file://*")
 
 # Fonts
@@ -57,13 +57,15 @@ c.content.autoplay = False
 c.content.webrtc_ip_handling_policy = "disable-non-proxied-udp"
 c.content.canvas_reading = False
 c.content.geolocation = False
-c.content.cookies.accept = "no-3rdparty"
+c.content.cookies.accept = "no-unknown-3rdparty"
 c.content.cookies.store = True
 
 # Content Blocking
 c.content.blocking.enabled = True
 c.content.blocking.method = "both"
 c.content.blocking.adblock.lists = [
+    "https://easylist.to/easylist/easylist.txt",
+    "https://easylist.to/easylist/easyprivacy.txt",
     "https://github.com/ewpratten/youtube_ad_blocklist/blob/master/blocklist.txt",
     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
@@ -81,12 +83,13 @@ c.content.blocking.adblock.lists = [
     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt",
-    "https://github.com/uBlockOrigin/uAssets/blob/master/filters/lan-block.txt",
     "https://github.com/yokoffing/filterlists/blob/main/annoyance_list.txt",
     "https://gitflic.ru/project/magnolia1234/bypass-paywalls-clean-filters/blob/?file=bpc-paywall-filter.txt&branch=main",
     "https://github.com/hagezi/dns-blocklists/blob/main/adblock/pro.mini.txt",
     "https://raw.githubusercontent.com/DandelionSprout/adfilt/refs/heads/master/LegitimateURLShortener.txt",
     "https://raw.githubusercontent.com/DandelionSprout/adfilt/refs/heads/master/BrowseWebsitesWithoutLoggingIn.txt",
+    "https://www.i-dont-care-about-cookies.eu/abp/",
+    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
 ]
 
 # Keybindings
@@ -100,4 +103,5 @@ config.bind("pP", "open -- {primary}")
 config.bind("pp", "open -- {clipboard}")
 config.bind("pt", "open -t -- {clipboard}")
 config.bind("qm", "macro-record")
+config.bind("pw", "spawn --detach proton-pass")
 config.bind(",j", "spawn --output-messages --userscript format_json.py")
