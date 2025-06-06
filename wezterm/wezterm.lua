41c518c51b0bfb8326099fb2e5678595a21f8fef
Aaliyah Harper <aeverly14@pm.me>
2025-06-06 09:36:55 -0400

    switch to lazyvim

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ config.font = wezterm.font('BerkeleyMono Nerd Font')
 config.font_size = 16
 config.color_scheme = 'Oxocarbon Dark (Gogh)'
 config.enable_tab_bar = false
-config.window_background_opacity = 0.97
+config.window_background_opacity = 0.99
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = "NONE"
 
