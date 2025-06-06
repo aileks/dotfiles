b7a63b14338cf7eca95caad92c21672c0e07b509
Aaliyah Harper <aeverly14@pm.me>
2025-06-06 09:57:22 -0400

    proprer transparancy in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,8 @@
 return {
   {
     "nyoom-engineering/oxocarbon.nvim",
-    opts = {
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" }),
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }),
-      vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" }),
-    },
+    lazy = false,
+    priority = 1000,
   },
   {
     "LazyVim/LazyVim",
