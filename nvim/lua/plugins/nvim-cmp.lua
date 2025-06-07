10f090e71ea8b90b9a65b322b0cf08c49868c29d
Aaliyah Harper <aeverly14@pm.me>
2025-06-07 09:47:15 -0400

    custom settings

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,7 +1,10 @@
 return {
   "hrsh7th/nvim-cmp",
+  dependencies = { "hrsh7th/cmp-emoji" },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
+    table.insert(opts.sources, { name = "emoji" })
+
     local has_words_before = function()
       unpack = unpack or table.unpack
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,8 @@
+return {
+  "mrjones2014/snacks.nvim",
+  opts = {
+    explorer = {
+      open_on_startup = false,
+    },
+  },
+}
