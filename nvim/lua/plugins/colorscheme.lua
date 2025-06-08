8b53bbe414345e038142e3f5820078f5200fa2f2
Aaliyah Harper <aeverly14@pm.me>
2025-06-08 08:24:42 -0400

    colorscheme tweaks

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,13 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      contrast = "hard",
+      transparent_mode = true,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
