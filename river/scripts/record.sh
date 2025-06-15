90c98ed33c834479c37a1d2d50b90e77e6a13f11
Aaliyah Harper <aeverly14@pm.me>
2025-06-15 09:41:47 -0400

    add screen recording script

---
+++ river/scripts/record.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+VIDEOS_DIR="$HOME/Videos/Recordings"
+mkdir -p "$VIDEOS_DIR"
+
+PID_FILE="/tmp/wf-recorder.pid"
+
+if [ -f "$PID_FILE" ] && pgrep -F "$PID_FILE" >/dev/null; then
+	pkill -F "$PID_FILE" -INT
+	rm "$PID_FILE"
+	notify-send "Screen recording stopped."
+else
+	GEOMETRY=$(slurp)
+
+	if [ -z "$GEOMETRY" ]; then
+		exit 0
+	fi
+
+	wf-recorder --audio -g "$GEOMETRY" -f "$VIDEOS_DIR/$(date +'%Y-%m-%d_%H-%M-%S').mp4" &
+	echo $! >"$PID_FILE"
+	notify-send "Screen recording started."
+fi
