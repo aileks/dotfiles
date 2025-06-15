399ed5deb5fe7395b88c23b4f893195426f4053e
Aaliyah Harper <aeverly14@pm.me>
2025-06-14 23:19:05 -0400

    found the problem

--- river/keybinds.sh
+++ river/keybinds.sh
@@ -12,13 +12,13 @@ riverctl map normal Super Escape spawn 'swaylock -f -i ~/Pictures/Wallpapers/dia
 riverctl map normal Super F toggle-fullscreen
 
 # # screenshots and recording
-# riverctl map normal Print spawn 'grim -g "$(slurp)" - | wl-copy'
-riverctl map normal Super Print spawn 'grim -g "$(slurp)" ~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png'
-# riverctl map normal Super+Shift R spawn 'bash -c '\''wf-recorder -g "$(slurp)" -f ~/recording_$(date +%Y-%m-%d_%H-%M-%S).mp4 &> /dev/null & echo $! > /tmp/wf-recorder.pid'\'''
-# riverctl map normal Super+Shift Escape spawn 'bash -c "kill -SIGINT $(cat /tmp/wf-recorder.pid 2>/dev/null) || pkill wf-recorder; rm -f /tmp/wf-recorder.pid"'
+riverctl map normal None Print spawn "grim -g '$(slurp)' - | wl-copy"
+riverctl map normal Super Print spawn "grim -g '$(slurp)' ~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png"
+riverctl map normal Super+Shift R spawn "wf-recorder -g '$(slurp)' -f ~/recording_$(date +%Y-%m-%d_%H-%M-%S).mp4 &> /dev/null & echo $! > /tmp/wf-recorder.pid"
+riverctl map normal Super+Shift Escape spawn "kill -SIGINT $(cat /tmp/wf-recorder.pid 2>/dev/null) || pkill wf-recorder; rm -f /tmp/wf-recorder.pid"
 
 # tag management
-for i in $(seq 1 7); do
+for i in $(seq 1 9); do
 	tags=$((1 << ($i - 1)))
 	riverctl map normal Super $i set-focused-tags $tags
 	riverctl map normal Super+Shift $i set-view-tags $tags
