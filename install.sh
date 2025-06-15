52a1061afd1a685b6722278067ed47663e0833e6
Aaliyah Harper <aeverly14@pm.me>
2025-06-15 19:30:02 -0400

    update gsettings commands

--- install.sh
+++ install.sh
@@ -21,8 +21,8 @@ ln -s $DOTDIR/rofi $HOME/.config/rofi
 rm -rf $HOME/.config/dunst
 ln -s $DOTDIR/dunst $HOME/.config/dunst
 
-rm -rf $HOME/.config/ghostty
-ln -s $DOTDIR/ghostty $HOME/.config/ghostty
+rm -rf $HOME/.config/kitty
+ln -s $DOTDIR/kitty $HOME/.config/kitty
 
 rm -rf $HOME/.config/fastfetch
 ln -s $DOTDIR/fastfetch $HOME/.config/fastfetch
