29f3e6826f456f93c0a795e6736bf9cb3e130643
Aaliyah Harper <aeverly14@pm.me>
2025-06-15 14:10:25 -0400

    switch to ghostty

--- install.sh
+++ install.sh
@@ -21,8 +21,8 @@ ln -s $DOTDIR/rofi $HOME/.config/rofi
 rm -rf $HOME/.config/dunst
 ln -s $DOTDIR/dunst $HOME/.config/dunst
 
-rm -rf $HOME/.config/alacritty
-ln -s $DOTDIR/alacritty $HOME/.config/alacritty
+rm -rf $HOME/.config/ghostty
+ln -s $DOTDIR/ghostty $HOME/.config/ghostty
 
 rm -rf $HOME/.config/fastfetch
 ln -s $DOTDIR/fastfetch $HOME/.config/fastfetch
