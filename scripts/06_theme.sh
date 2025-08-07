09dd9cd07ea034f4b43113bbea2a33c7e366e6fe
Aaliyah Harper <aeverly14@pm.me>
2025-08-07 18:01:33 -0400

    remove directories

--- scripts/06_theme.sh
+++ scripts/06_theme.sh
@@ -3,12 +3,14 @@
 # GTK Theme
 git clone https://github.com/Fausto-Korpsvart/Gruvbox-GTK-Theme.git /tmp/gruvbox
 /tmp/gruvbox/themes/install.sh -s compact -c dark -d ~/.local/share/themes -l -t teal --tweaks float
+rm -rf /tmp/gruvbox
 
 # Icon Theme
 sudo pacman -S --noconfirm --needed papirus-icon-theme
 git clone https://github.com/xelser/gruvbox-papirus-folders.git /tmp/icons
 sudo cp -r /tmp/icons/src/* /usr/share/icons/Papirus
 /tmp/icons/papirus-folders -C gruvbox-original-purple --theme Papirus-Dark
+rm -rf /tmp/icons
 
 # Extras
 yay -S --noconfirm --needed plymouth-theme-black-hud-git posy-cursors
