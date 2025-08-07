5393edd9af159a79cea551368c735777c9d4c007
Aaliyah Harper <aeverly14@pm.me>
2025-08-07 18:56:20 -0400

    add missing font

--- scripts/07_fonts.sh
+++ scripts/07_fonts.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-yay -S --noconfirm --needed ttf-adwaita-mono-nerd ttf-font-awesome noto-fonts-emoji noto-fonts-cjk ttf-ia-writer otf-stix
+yay -S --noconfirm --needed ttf-adwaita-mono-nerd ttf-font-awesome noto-fonts-emoji noto-fonts-cjk ttf-ia-writer otf-stix ttf-literata-meta
