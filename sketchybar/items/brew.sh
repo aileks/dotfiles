3d307c4534ca4ac9fa11d9f0d9c06cc279d279a6
Aaliyah Harper <aaliyah@aileks.dev>
2025-09-10 16:30:48 -0400

    Add brew_update event and subscribe brew item in SketchyBar and alias to trigger it

--- sketchybar/items/brew.sh
+++ sketchybar/items/brew.sh
@@ -4,7 +4,10 @@ source "$CONFIG_DIR/colors.sh"
 
 PLUGIN_DIR="$CONFIG_DIR/plugins"
 
+sketchybar --add event brew_update
+
 sketchybar --add item brew right \
-  --set brew update_freq=14400 \
-  script="$PLUGIN_DIR/brew.sh" \
-  background.color=$BG1
+           --set brew update_freq=14400 \
+           script="$PLUGIN_DIR/brew.sh" \
+           background.color=$BG1 \
+           --subscribe brew brew_update
--- sketchybar/plugins/brew.sh
+++ sketchybar/plugins/brew.sh
@@ -3,17 +3,17 @@
 source "$CONFIG_DIR/colors.sh"
 
 sketchybar --set "$NAME" label="Checking for updates..." \
-                         icon=􀐚 \
-                         icon.color=$ORANGE \
+                            icon=􀐚 \
+                            icon.color=$ORANGE \
 
 COUNT=$(brew outdated 2>/dev/null | wc -l | xargs)
 
 if [ "$COUNT" -eq 0 ]; then
     sketchybar --set "$NAME" label="􀆅" \
-                             icon=􀐚 \
-                             label.color=$GREEN
+                                icon=􀐚 \
+                                label.color=$GREEN
 else
     sketchybar --set "$NAME" label="$COUNT updates" \
-                             icon=􀐚 \
-                             label.color=$YELLOW
+                                icon=􀐚 \
+                                label.color=$YELLOW
 fi
