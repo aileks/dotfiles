9fc8b2cd2a29491fd083db063a7c5cd2c78f5107
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-08 18:39:41 -0500

    fix: remove invalid #L prefix from hex color codes across configs

---
+++ wezterm/ashen.lua
@@ -0,0 +1,20 @@
+-- Ashen theme for wezterm (github.com/ficcdaf/ashen.nvim)
+-- Author: Daniel Fichtinger
+-- License: MIT
+return {
+  foreground = "#f5f5f5",
+  background = "#121212",
+
+  cursor_bg = "#f5f5f5",
+  cursor_fg = "#121212",
+  cursor_border = "#f5f5f5",
+
+  selection_fg = "#f5f5f5",
+  selection_bg = "#1d1d1d",
+
+  scrollbar_thumb = "#121212",
+  split = "#121212",
+
+  ansi = { "#121212", "#B14242", "#D87C4A", "#E49A44", "#4A8B8B", "#a7a7a7", "#b4b4b4", "#d5d5d5" },
+  brights = { "#949494", "#B14242", "#D87C4A", "#E49A44", "#4A8B8B", "#a7a7a7", "#b4b4b4", "#d5d5d5" },
+}
