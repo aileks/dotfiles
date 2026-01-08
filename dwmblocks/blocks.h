f384a957c2630f0876a007abdf53b9553c00d295
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-08 16:39:39 -0500

    add dwm/dwmblocks config and statusbar scripts with Ashen colors

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,10 +1,9 @@
 static const Block blocks[] = {
-	{"",    "sb-cpu",      10,    2},
-	{"",    "sb-memory",   10,    3},
-	{"",    "sb-volume",   0,     10},
-	{"",    "sb-network",  5,     4},
-	{"",    "sb-battery",  30,    5},
-	{"",    "sb-clock",    60,    1},
+	{"",    "cpu",      10,    2},
+	{"",    "memory",   10,    3},
+	{"",    "volume",   0,     10},
+	{"",    "network",  5,     4},
+	{"",    "clock",    60,    1},
 };
 
 static char delim[] = " ^c#535353^|^d^ ";
