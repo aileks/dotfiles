f3000bda083760557dd23910a85828508a160922
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-14 00:20:57 -0500

    update path & patch dwmblocks with statuscmd

--- dwmblocks/dwmblocks.c
+++ dwmblocks/dwmblocks.c
@@ -28,6 +28,7 @@ typedef struct {
 void dummysighandler(int num);
 #endif
 void sighandler(int num);
+void buttonhandler(int sig, siginfo_t *si, void *ucontext);
 void getcmds(int time);
 void getsigcmds(unsigned int signal);
 void setupsignals();
@@ -52,19 +53,36 @@ static void (*writestatus) () = pstdout;
 
 static char statusbar[LENGTH(blocks)][CMDLENGTH] = {0};
 static char statusstr[2][STATUSLENGTH];
+static char button[] = "\0";
 static int statusContinue = 1;
 static int returnStatus = 0;
 
 //opens process *cmd and stores output in *output
 void getcmd(const Block *block, char *output)
 {
-	//make sure status is same until output is ready
+	if (block->signal)
+	{
+		output[0] = block->signal;
+		output++;
+	}
 	char tempstatus[CMDLENGTH] = {0};
 	strcpy(tempstatus, block->icon);
-	FILE *cmdf = popen(block->command, "r");
+	FILE *cmdf;
+	if (*button)
+	{
+		setenv("BUTTON", button, 1);
+		cmdf = popen(block->command,"r");
+		*button = '\0';
+		unsetenv("BUTTON");
+	}
+	else
+	{
+		cmdf = popen(block->command,"r");
+	}
 	if (!cmdf)
 		return;
 	int i = strlen(block->icon);
+
 	fgets(tempstatus+i, CMDLENGTH-i-delimLen, cmdf);
 	i = strlen(tempstatus);
 	//if block and command output are both not empty
@@ -109,10 +127,17 @@ void setupsignals()
         signal(i, dummysighandler);
 #endif
 
+	struct sigaction sa;
+	sigemptyset(&sa.sa_mask);
 	for (unsigned int i = 0; i < LENGTH(blocks); i++) {
-		if (blocks[i].signal > 0)
+		if (blocks[i].signal > 0) {
 			signal(SIGMINUS+blocks[i].signal, sighandler);
+			sigaddset(&sa.sa_mask, SIGRTMIN+blocks[i].signal);
+		}
 	}
+	sa.sa_sigaction = buttonhandler;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
 
 }
 
@@ -176,6 +201,13 @@ void statusloop()
 void dummysighandler(int signum)
 {
     return;
+}
+
+void buttonhandler(int sig, siginfo_t *si, void *ucontext)
+{
+	*button = '0' + (si->si_value.sival_int & 0xff);
+	getsigcmds(si->si_value.sival_int >> 8);
+	writestatus();
 }
 #endif
 
--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export PATH="$HOME/.local/bin:/usr/local/bin:/usr/bin:/usr/sbin:/bin:$PATH"
 export XCURSOR_SIZE=48
 
 xrdb -merge ~/.Xresources &
