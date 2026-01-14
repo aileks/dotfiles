356a8271e883bceeae5b5147be2a5d639dbeb2e0
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-13 19:04:57 -0500

    add yazi theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,28 +1,21 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("BerkeleyMono Nerd Font Mono")
 -- Alternative: config.font = wezterm.font("AdwaitaMono Nerd Font Mono")
-config.font_size = 12.0
+config.font = wezterm.font("BerkeleyMono Nerd Font Mono")
+config.font_size = 14.0
 config.colors = require("ashen")
-
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
 config.window_background_opacity = 0.95
+config.audible_bell = "Disabled"
+config.scrollback_lines = 10000
 config.window_padding = {
   left = 10,
   right = 10,
   top = 10,
   bottom = 10,
 }
-
-config.enable_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
-config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = false
-
-config.window_decorations = "RESIZE"
-config.window_close_confirmation = "NeverPrompt"
-
-config.audible_bell = "Disabled"
-config.scrollback_lines = 10000
 
 return config
