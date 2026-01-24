69b7de76ad3860a6f6bc32b98851e531aac19515
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-24 17:37:17 -0500

    more font scaling shenanigans

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("CommitMono Nerd Font Mono")
-config.font_size = 16
+config.font_size = 9
 config.colors = require("ashen")
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
