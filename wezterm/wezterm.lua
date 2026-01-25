282c165634d4b20a619166ea0cb0c5979f34d2d3
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-25 15:09:34 -0500

    update font sizes due to scaling changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,12 +2,12 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("CommitMono Nerd Font Mono")
-config.font_size = 14
+config.font_size = 18
 config.colors = require("ashen")
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
-config.window_background_opacity = 0.99
+config.window_background_opacity = 0.98
 config.audible_bell = "Disabled"
 config.scrollback_lines = 10000
 config.window_padding = {
