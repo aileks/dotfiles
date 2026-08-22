254d18ad1a928200e2b875faf59e30c805ae1bdc
Kieran Pedrick <kieran@aileks.dev>
2026-08-21 21:26:57 -0400

    fix monitor positions

--- hypr/monitors.lua
+++ hypr/monitors.lua
@@ -3,7 +3,7 @@ local omarchy_gdk_scale = 1
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
 
 hl.monitor({ output = "DP-1", mode = "2560x1440@200", position = "0x0", scale = 1 })
-hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@200", position = "auto-left", scale = 1, transform = 1 })
+hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@200", position = "-1080x-240", scale = 1, transform = 1 })
 
 for workspace = 1, 7 do
   hl.workspace_rule({
