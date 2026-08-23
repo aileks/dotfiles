8f037d8694d936a87503d0b2d0eb99276ac1f1b4
Kieran Pedrick <kieran@aileks.dev>
2026-08-23 16:16:00 -0400

    convert nvim directory to a submodule

--- nvim/init.lua
+++
@@ -1,8 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-require('config.options')
-require('config.keymaps')
-require('config.autocmds')
-require('config.terminal')
-require('plugins')
--- nvim/lua/plugins/lazygit.lua
+++
@@ -1,4 +0,0 @@
-local map = vim.keymap.set
-
-map('n', '<leader>gg', '<cmd>LazyGit<cr>', { desc = 'Open LazyGit' })
-map('n', '<leader>gf', '<cmd>LazyGitCurrentFile<cr>', { desc = 'LazyGit current file history' })
