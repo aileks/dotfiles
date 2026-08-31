6bec2b806c0cd12a724325c70b6808db77f9b34f
Kieran Pedrick <kieran@aileks.dev>
2026-08-30 22:29:24 -0400

    add hyprpicker keybind & focus on activate

--- hypr/settings.lua
+++ hypr/settings.lua
@@ -56,6 +56,7 @@ hl.config({
 		workspace_swipe_distance = 500,
 	},
 	misc = {
+		focus_on_activate,
 		disable_hyprland_logo = true,
 		disable_splash_rendering = true,
 		focus_on_activate = false,
