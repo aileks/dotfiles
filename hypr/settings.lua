3543eee07a4d3d450de491c0e4f1046e40077e63
Kieran Pedrick <kieran@aileks.dev>
2026-09-01 21:50:03 -0400

    focus on activate

--- hypr/settings.lua
+++ hypr/settings.lua
@@ -56,10 +56,9 @@ hl.config({
 		workspace_swipe_distance = 500,
 	},
 	misc = {
-		focus_on_activate,
 		disable_hyprland_logo = true,
 		disable_splash_rendering = true,
-		focus_on_activate = false,
+		focus_on_activate = true,
 		animate_manual_resizes = true,
 		enable_swallow = true,
 		swallow_regex = "^(kitty)$",
