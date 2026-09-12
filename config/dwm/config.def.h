53a471b696e054f487c9ece9053f06684831462b
Kieran Pedrick <kieran@aileks.dev>
2026-09-12 19:05:31 -0400

    remove audio plugin launchers and generic app aliases

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -896,7 +896,7 @@ static const char *xkb_layouts[]  = {
 #if !NODMENU_PATCH
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 #endif // NODMENU_PATCH
-static const char *dmenucmd[] = { "j4-dmenu-desktop", "--dmenu", "dmenu -i -p Apps", "--term", "wezterm", NULL };
+static const char *dmenucmd[] = { "j4-dmenu-desktop", "--no-generic", "--dmenu", "dmenu -i -p Apps", "--term", "wezterm", NULL };
 static const char *termcmd[]  = { "wezterm", "connect", "unix", NULL };
 
 #if BAR_STATUSCMD_PATCH
