97ffa72dba2d60be7e4509cfaeee8e32ca9c0119
Kieran Pedrick <kieran@aileks.dev>
2026-09-14 13:19:46 -0400

    decrease wezterm opacity

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -96,7 +96,7 @@ local settings = {
     }
   },
   ["use_fancy_tab_bar"] = false,
-  ["window_background_opacity"] = 0.95,
+  ["window_background_opacity"] = 0.97,
   ["window_close_confirmation"] = "NeverPrompt",
   ["window_decorations"] = "NONE"
 }
