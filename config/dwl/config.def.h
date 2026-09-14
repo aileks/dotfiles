87a210de63b57399b7b69287320bdc198e139565
Kieran Pedrick <kieran@aileks.dev>
2026-09-14 14:49:17 -0400

    smaller font size

--- config/dwl/config.def.h
+++ config/dwl/config.def.h
@@ -15,7 +15,7 @@ static const int showsystray               = 1; /* 0 means no systray */
 static const char *icon_theme              = "Papirus-Dark";
 static const int showbar                   = 1; /* 0 means no bar */
 static const int topbar                    = 1; /* 0 means bottom bar */
-static const char *fonts[]                 = {"Iosevka Nerd Font Propo:size=12"};
+static const char *fonts[]                 = {"Iosevka Nerd Font Propo:size=11"};
 static const float rootcolor[]             = COLOR(0x131210ff);
 static uint32_t colors[][3]                = {
 	/*               fg          bg          border    */
