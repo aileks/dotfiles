0feb4f472c45352bd6577bdfa31e16ab00b81e72
Kieran Pedrick <kieran@aileks.dev>
2026-09-16 12:50:13 -0400

    package.use consolidation

--- install.sh
+++ install.sh
@@ -343,32 +343,11 @@ install_system_file() {
 
 install_system_config() {
   local source relative
-  local manifest=/var/lib/dotfiles/etc-manifest
 
   while IFS= read -r -d '' source; do
     relative=${source#"$repo/"}
     install_system_file "$source" "/$relative"
   done < <(find "$repo/etc" -type f -print0 | sort -z)
-
-  # Remove live files a previous run laid down whose repo counterpart has
-  # since been deleted. Only manifest-tracked paths are considered, so
-  # hand-maintained files under /etc are never pruned.
-  if [[ -r $manifest ]]; then
-    while IFS= read -r relative; do
-      [[ -e /etc/$relative || -L /etc/$relative ]] || continue
-      [[ -e $repo/etc/$relative ]] && continue
-      run install -d -m 700 -- "/var/backups/dotfiles/$stamp/removed/etc/${relative%/*}"
-      run cp -a -- "/etc/$relative" "/var/backups/dotfiles/$stamp/removed/etc/$relative"
-      run rm -f -- "/etc/$relative"
-    done < "$manifest"
-  fi
-
-  if "$dry_run"; then
-    printf 'update %s\n' "$manifest"
-  else
-    run install -d -m 755 -- /var/lib/dotfiles
-    find "$repo/etc" -type f -printf '%P\n' | LC_ALL=C sort >"$manifest"
-  fi
 
   install_system_file "$repo/config/qt6ct/colors/cinder-grove.conf" /usr/local/share/qt6ct/colors/cinder-grove.conf
   if [[ -f /etc/ly/config.ini ]] && grep -Eq '^[[:space:]]*login_cmd[[:space:]]*=[[:space:]]*/usr/local/bin/start-session[[:space:]]*$' /etc/ly/config.ini; then
