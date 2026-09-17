d09f151af1f9466f2d014f16b3b776adc695a152
Kieran Pedrick <kieran@aileks.dev>
2026-09-17 11:16:27 -0400

    picom tweaks and such

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -72,7 +72,7 @@ config.colors = {
 }
 
 config.use_fancy_tab_bar = false
-config.window_background_opacity = 0.97
+config.window_background_opacity = 0.95
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = 'NONE'
 config.window_padding = {
