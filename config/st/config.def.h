6bbf03cad55d55571fe4dae146c03cc1edf9165c
Kieran Pedrick <kieran@aileks.dev>
2026-09-17 21:58:08 -0400

    normal nerd font over nfm

--- config/st/config.def.h
+++ config/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IosevkaTerm Nerd Font Mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "IosevkaTerm Nerd Font:pixelsize=20:antialias=true:autohint=true";
 
 static int borderpx = 8;
 
