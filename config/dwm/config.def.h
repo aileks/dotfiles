149f6802327e58836704a5940ff0ede96ad5c321
Kieran Pedrick <kieran@aileks.dev>
2026-09-20 15:28:12 -0400

    no need to launch signal with these flags

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -224,7 +224,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_space,      spawn,                  SHCMD("j4-dmenu-desktop --dmenu='dmenu -i -c -l 8 -p Apps -S' --display-binary --term st --usage-log=$HOME/.cache/j4-history") },
 	{ MODKEY,                       XK_x,          spawn,                  SHCMD("emacsclient -c -a ''") },
 	{ MODKEY,                       XK_w,          spawn,                  SHCMD("zen-browser") },
-	{ MODKEY,                       XK_s,          spawn,                  SHCMD("signal-desktop --ignore-gpu-blocklist --enable-features=AcceleratedVideoDecodeLinuxGL --use-gl=desktop") },
+	{ MODKEY,                       XK_s,          spawn,                  SHCMD("signal-desktop") },
 	{ MODKEY,                       XK_e,          spawn,                  SHCMD("st -e open-nnn") },
 	{ MODKEY,                       XK_a,          spawn,                  SHCMD("st -e wiremix") },
 	{ MODKEY,                       XK_o,          spawn,                  SHCMD("region-ocr") },
