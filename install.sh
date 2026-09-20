d9aa4a40eb837b502ac73a2a34558c771506de44
Kieran Pedrick <kieran@aileks.dev>
2026-09-20 11:05:25 -0400

    add imagemagick

--- install.sh
+++ install.sh
@@ -139,7 +139,7 @@ packages=(
   libX11-devel libXcursor-devel libXext-devel libXft-devel libXinerama-devel libXrandr-devel libXrender-devel libxcb-devel
   libxcrypt-devel xcb-util-devel clang gcc gdb make bubblewrap cmake neovim libvterm tree-sitter-cli libgccjit-devel
   jansson-devel tree-sitter-devel gtk+3-devel cairo-devel harfbuzz-devel giflib-devel libjpeg-turbo-devel libpng-devel
-  librsvg-devel libwebp-devel libxml2-devel gnutls-devel texinfo autoconf automake libXpm-devel ncurses-devel
+  librsvg-devel libwebp-devel libxml2-devel gnutls-devel texinfo autoconf automake libXpm-devel ncurses-devel ImageMagick
 )
 
 custom_packages=(
